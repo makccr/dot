12b895eb364d8eaccdff87dd4c88986c7371e344
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2020-06-05 21:48:40 -0500

    switcing default terminal back to kitty;

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -32,7 +32,7 @@ end
 
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
-terminal = "termite"
+terminal = "kitty"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
 modkey = "Mod4"
