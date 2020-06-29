9591acd8bf96e77f09f23e8d4c17862e536d1a77
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2020-06-28 22:43:21 -0500

    Switching to just using polybar on one monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,10 +4,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar makc &
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload makc &
-  done
-else
-  polybar --reload makc &
-fi
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload makc &
+#  done
+#else
+#  polybar --reload makc &
+#fi
