bf697cb560eae18b42ff7c59c98cf1ac871132a8
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2020-08-12 20:43:06 -0500

    Removing applets from launching, I'm working on a different solution

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -369,8 +369,6 @@ beautiful.useless_gap = 10
 -- Autostart
 awful.spawn.with_shell("picom -CG --experimental-backends")
 awful.spawn.with_shell("dropbox")
-awful.spawn.with_shell("kmix")
 awful.spawn.with_shell("ntfd")
-awful.spawn.with_shell("nm-applet")
 awful.spawn.with_shell("/home/makc/.config/polybar/launch.sh")
 awful.spawn.with_shell("feh --bg-fill --randomize ~/Media/wallpapers-ii/wallpapers")
