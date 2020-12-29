a5a857bc6d6041f0a57b6894d0bdcf5480ee45b1
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2020-12-29 09:07:20 -0600

    Adding a blurred background to windows with tryone-picom

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -532,7 +532,7 @@ beautiful.useless_gap = 5
 
 -- Autostart
 --awful.spawn.with_shell("picom -CG --experimental-backends")
-awful.spawn.with_shell("picom")
+awful.spawn.with_shell("picom --experimental-backends")
 awful.spawn.with_shell("dropbox")
 awful.spawn.with_shell("ntfd")
 awful.spawn.with_shell("nm-applet")
