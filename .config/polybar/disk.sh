cc753f303f64fcf075e7e2dc3844582ae7b35cd1
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2021-09-28 00:57:02 -0500

    Simplifying disk polybar module

--- .config/polybar/disk.sh
+++ .config/polybar/disk.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-percent=$( df -H / | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 }' )
-spaceRemain=$( df -H / | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $4 }' )
+percent=$( df -H / | grep -vE '^Filesystem' | awk '{ print $5 }' )
+spaceRemain=$( df -H / | grep -vE '^Filesystem' | awk '{ print $4 }' )
 
 echo "$percent • $spaceRemain"
