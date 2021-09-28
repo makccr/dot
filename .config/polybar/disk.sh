3667d552bd0d8d801159c2ac4e97f9e7f6fbc2f0
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2021-09-28 14:11:30 -0500

    Tweaking syntax

--- .config/polybar/disk.sh
+++ .config/polybar/disk.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 percent=$( # Pulling percent of drive used
-    df -H / | grep -vE '^Filesystem' | awk '{ print $5 }' 
-    )
+    df -H / | grep -vE '^Filesystem' | awk '{ print $5 }' )
 
 spaceRemain=$( # Pulling storage in gigabytes left
-    df -H / | grep -vE '^Filesystem' | awk '{ print $4 }' 
-    )
+    df -H / | grep -vE '^Filesystem' | awk '{ print $4 }' )
 
 # Printing both percentage used, and space remaining together
 echo "$percent • $spaceRemain"
