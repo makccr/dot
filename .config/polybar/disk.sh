acafdaddee61ca76a5bcd5d127f17f4f8fddc30a
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2021-10-03 16:13:39 -0500

    Fucking with polybar

--- .config/polybar/disk.sh
+++ .config/polybar/disk.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-percent=$( # Pulling percent of drive used
-    df -H / | grep -vE '^Filesystem' | awk '{ print $5 }' )
+percent=$(
+    df -H / | grep -vE '^Filesystem' | awk '{ print $5}' 
+    )
 
-spaceRemain=$( # Pulling storage in gigabytes left
-    df -H / | grep -vE '^Filesystem' | awk '{ print $4 }' )
-
-# Printing both percentage used, and space remaining together
-echo "$percent • $spaceRemain"
+remaining=$(
+    df -H / | grep -vE '^Filesystem' | awk '{ print $4}' 
+    )
+echo "$percent • $remaining"
