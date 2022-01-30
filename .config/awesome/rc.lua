4b6135c948cdee0221af1b334108c6251f4f2743
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2022-01-30 10:22:42 -0600

    Switching back to nine tags, and tweaking polybar colors

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -170,8 +170,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5" }, s, awful.layout.layouts[1])
-    --awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
 --    s.mypromptbox = awful.widget.prompt()
