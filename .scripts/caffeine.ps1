cf4393d5d973bfd0ea9ec95c37b8e5147fc0679f
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2025-07-07 02:26:22 -0500

    Adding a script to prevent computer from falling asleep on Windows

---
+++ .scripts/caffeine.ps1
@@ -0,0 +1,17 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+function Press-Spacebar {
+    [System.Windows.Forms.SendKeys]::SendWait(" ")
+}
+
+while ($true) {
+    $delay = Get-Random -Minimum 5 -Maximum 300  # Random delay between 5 and 300 seconds
+    Start-Sleep -Seconds $delay
+
+    Press-Spacebar
+    Write-Output "Pressed spacebar after waiting $delay seconds at $(Get-Date)"
+}
+
+# RUN SCRIPT WITH COMMAND BELOW
+# powershell -ExecutionPolicy Bypass -File .\caffeine.ps1
