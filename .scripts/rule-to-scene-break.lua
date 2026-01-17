5e1e3bd12f39fe425d9412ed479dba676ef82d60
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2026-01-17 13:39:57 -0600

    Fixing this so it actually works

--- .scripts/rule-to-scene-break.lua
+++ .scripts/rule-to-scene-break.lua
@@ -1,4 +1,7 @@
 -- A function to print HorizontalRule's in Markdown as Scene breaks when exporting to .odt or .docx
 function HorizontalRule()
-    return pandoc.Para({pandoc.Str("#")}, {class="SceneBreak"})
+    -- convert HR to #
+    local para = pandoc.Para({pandoc.Str("#")})
+    -- apple "SceneBreak" style to the hash
+    return pandoc.Div({para}, pandoc.Attr("", {}, {["custom-style"]="SceneBreak"}))
 end
