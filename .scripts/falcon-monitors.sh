f159a2ccac60007731f2ee0c45d3cb32d04f0fa0
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2026-01-22 09:31:06 -0600

    removing old, unused bash scripts

--- .scripts/falcon-monitors.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --off --output DP-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off
--- .scripts/fix.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --off
