9161d62d4787bb2e07c3882d5811b20b41d70025
Mackenzie Criswell <m@makc.co>
2026-09-07 00:45:04 -0500

    Swapping default term to foot

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -28,7 +28,7 @@ hl.monitor({
 ---------------------
 
 -- Set programs that you use
-local terminal    = "alacritty"
+local terminal    = "foot"
 local browser     = "firefox"
 local menu        = "dmenu_run"
 
@@ -290,7 +290,7 @@ hl.bind(mainMod .. " + SHIFT + L", hl.dsp.exec_cmd("hyprlock"))
 -- hyprshot
 hl.bind(mainMod .. " + PRINT", hl.dsp.exec_cmd("hyprshot -m output"))
 -- Wallpaper Changin' Scripy-thang
-hl.bind  (mainMod .. " + C", hl.dsp.exec_cmd("~/.scripts/wallpaper/landscape.sh"))
+hl.bind  (mainMod .. " + C", hl.dsp.exec_cmd("~/.scripts/wallpaper/space.sh"))
 
 -- Switch workspaces with mainMod + [0-9]
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
